23fdbdc78732aeb86dabc02029b2b3ad793057fb
wangmaoxuPCX1 <guangyuanwangmaoxu@163.com>
2019-10-20 14:48:07 +0800

    update

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ from import_sets import *
 if __name__ == '__main__':
     for iter, model in enumerate(options["general"]["pretrainedmodelpath"]):
         model = torch.load(options["general"]["pretrainedmodelpath"][iter])
-
+        
         # Move the model to the GPU.
         if (options["general"]["usecudnn"]):
             model = model.cuda(options["general"]["gpuid"])
@@ -25,19 +25,15 @@ if __name__ == '__main__':
                 input = Variable(sample['temporalvolume']).cuda()
                 samplename = sample['samplename']
                 output = model(input)
+                output = output[0].tolist()
+                output = output.index(max(output))
                 print("predict {} samples".format(i))
-                outputList.append([samplename, output])
-
-        # index 用来保存文件夹名和对应的预测值index
-        index = []
-        for i in range(len(outputList)):
-            data = outputList[i][1][0].tolist()
-            index.append([outputList[i][0][0], data.index(max(data))])
+                outputList.append([samplename[0], output])
 
         # 保存格式：文件夹名，预测值
         with open('outputList{}.csv'.format(iter), 'w', newline='', encoding='UTF-8') as f:
-            for i in range(len(index)):
+            for i in range(len(outputList)):
                 f_csv = csv.writer(f)
-                f_csv.writerow([index[i][0], map_index_hanzi[str(index[i][1])]])
+                f_csv.writerow([outputList[i][0], map_index_hanzi[str(outputList[i][1])]])
         print("test {} samples".format(len(predict_dataset_loader)))
 
